bbb6f7b064b7393282469a856943b1ed67058bde
Bhushan Bonde <u052731@Bhushans-MacBook.local>
2017-03-05 17:38:42 +0000

    initial commit

---
+++ routes.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+#	return "Hello World! Hi Vedant"
+
+@app.route("/about")
+def about():
+	return render_template("about.html")
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
